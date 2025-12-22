320fd698b765b69574ff7dfbe2611712c27f3c94
DRINK <2321866490@qq.com>
2025-12-22 17:12:06 +0800

    update challenge2.2

--- Paper/hotstorage26/challenge/2/1/time.R
+++ Paper/hotstorage26/challenge/2/1/time.R
@@ -1,21 +1,31 @@
+# 设置当前工作路径
+setwd("E:/Users/drinkwater/Desktop/R-script-template/Paper/hotstorage26/challenge/2/1/")
+
 # 加载绘图函数
 source("E:/Users/drinkwater/Desktop/R-script-template/MyR/Line_Point.R")
 
-# 读取数据
-data <- read.table("e:/Users/drinkwater/Desktop/R-script-template/Paper/hotstorage26/challenge/2/1/glibc.txt", header = TRUE)
+# 定义文件名和导出名
+files <- c("glibc.txt", "linux.txt", "web.txt", "windowsLog.txt")
+export_names <- c("glibc.pdf", "linux.pdf", "web.pdf", "windowsLog.pdf")
 
-# 只取第一列和第三列
-df <- data.frame(
-  x = data[[1]],
-  y = data[[3]]
-)
-
-# 画折线图（x轴为vector_size，y轴为cdf）
-plot_line_comparison(
-  data = data.frame(df$y), # 只画y，x轴用默认序号
-  x_label = "vector size",
-  y_label = "cdf",
-  export_path = "E:/Users/drinkwater/Desktop/R-script-template/Paper/hotstorage26/challenge/2/1/",
-  export_name = "glibc.pdf",
-  y_lim = c(0, 1)
-)
+# 循环处理每个文件
+for (i in seq_along(files)) {
+  # 读取数据
+  data <- read.table(files[i], header = TRUE)
+  
+  # 只取第一列和第三列
+  df <- data.frame(
+    x = data[[1]],
+    y = data[[3]]
+  )
+  
+  # 画折线图
+  plot_line_comparison(
+    data = data.frame(df$y), # 只画y，x轴用默认序号
+    x_label = "vector size",
+    y_label = "cdf",
+    export_path = getwd(),
+    export_name = export_names[i],
+    y_lim = c(0, 1)
+  )
+}
